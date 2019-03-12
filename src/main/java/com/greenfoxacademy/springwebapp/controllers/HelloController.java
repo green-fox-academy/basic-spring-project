43cfda20d765ecdcb8f25bea1752400abf5437ae
csadesz <csadesz@gmail.com>
2019-03-12 10:57:57 +0100

    fix(HelloController)add autowired annotation

--- src/main/java/com/greenfoxacademy/springwebapp/controllers/HelloController.java
+++ src/main/java/com/greenfoxacademy/springwebapp/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package com.greenfoxacademy.springwebapp.controllers;
 
 import com.greenfoxacademy.springwebapp.services.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ public class HelloController {
 
   private HelloService helloService;
 
+  @Autowired
   public HelloController(HelloService helloService){
     this.helloService = helloService;
   }
