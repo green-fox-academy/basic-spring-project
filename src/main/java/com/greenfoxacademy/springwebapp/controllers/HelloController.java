63b2e7209cacf095a4ef75accffe00489f76a2ed
csadesz <csadesz@gmail.com>
2019-03-12 10:35:05 +0100

    feat(HelloController): use service layer's method in helloWorld endpoint

--- src/main/java/com/greenfoxacademy/springwebapp/controllers/HelloController.java
+++ src/main/java/com/greenfoxacademy/springwebapp/controllers/HelloController.java
@@ -1,14 +1,22 @@
 package com.greenfoxacademy.springwebapp.controllers;
 
+import com.greenfoxacademy.springwebapp.services.HelloService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HelloController {
 
-  @GetMapping
-  public String helloWorld() {
-    return "hello";
+  private HelloService helloService;
+
+  public HelloController(HelloService helloService){
+    this.helloService = helloService;
   }
 
+  @GetMapping("/hello")
+  public String helloWorld() {
+    return helloService.getHelloWorld();
+  }
 }
